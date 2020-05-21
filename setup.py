f903a194ced50ff21ecba702cb02e1b40f4fe464
Tony Hirst <tony.hirst@gmail.com>
2020-05-21 07:30:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="nb_quality_profile",
+    packages=['nb_quality_profile'],
+    version='0.0.1',
+    author="Tony Hirst",
+    author_email="tony.hirst@gmail.com",
+    description="Tools for profiling Jupyter notebook quality and visualing notebook structure.",
+    long_description='''
+    A range of tools for profiling Jupyter notebooks and visualising notebook structure.
+    ''',
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'nbformat',
+        'matplotlib'
+    ],
+
+)
