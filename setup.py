8f5d20dd0bcf5f50e13e965c11a8a869b94740f8
Tony Hirst <tony.hirst@gmail.com>
2022-05-31 14:12:07 +0100

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
+
 setup(
     name="nb_quality_profile",
     packages=['nb_quality_profile'],
-    version='0.2.2',
+    version='0.2.3',
     author="Tony Hirst",
     author_email="tony.hirst@gmail.com",
     description="Tools for profiling Jupyter notebook quality and visualing notebook structure.",
@@ -11,14 +14,10 @@ setup(
     A range of tools for profiling Jupyter notebooks and visualising notebook structure.
     ''',
     long_description_content_type="text/markdown",
-    install_requires=[
-        'click',
-        'nbformat',
-        'matplotlib',
-        'list-imports',
-        'jupytext',
-        'readtime'
-    ],
+    install_requires=["nbformat", "jupytext", "click",
+                      "deepmerge", "lxml", "markdown",
+                      "matplotlib", "spacy", "textacy", "readtime",
+                      "list-imports", "pytest-codeblocks", "radon"],
     entry_points='''
         [console_scripts]
         nb_quality=nb_quality_profile.cli:cli
